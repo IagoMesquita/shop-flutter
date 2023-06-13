3f40cc09eea6a508c4838aafcda0e3ccd98076ad
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-12 21:58:53 -0300

    feat: class Product

---
+++ lib/models/product.dart
@@ -0,0 +1,21 @@
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFarovite;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    this.isFarovite = false,
+  });
+
+  void toggleFavorite() {
+    isFarovite = !isFarovite;
+  }
+}
