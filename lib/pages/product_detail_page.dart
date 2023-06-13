5b920211c2873a182a7b4c5eb48d1e96b99fda45
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-13 08:18:34 -0300

    refactor: from parametar constructor to ModalRoute

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:shop/models/product.dart';
 
 class ProductDetailPage extends StatelessWidget {
-
-  final Product product;
-
-  const ProductDetailPage({super.key, required this.product});
+  const ProductDetailPage({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
+  final Product product = ModalRoute.of(context)?.settings.arguments  as Product;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
