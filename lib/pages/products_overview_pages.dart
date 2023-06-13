7bb66ec7b1fa88b19f862a2ee7bba10d9503484a
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-12 22:44:15 -0300

    feat: GridView.builder for products overview

--- lib/pages/products_overview_pages.dart
+++ lib/pages/products_overview_pages.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:shop/components/product_item.dart';
+import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product.dart';
 
-class ProductOverviewPage extends StatelessWidget {
-  const ProductOverviewPage({super.key});
+class ProductsOverviewPage extends StatelessWidget {
+  final List<Product> loadedProducts = dummyProducts;
+
+  ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Minha Loja')),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+        ),
+        itemBuilder: (ctx, index) => ProductItem(product: loadedProducts[index]),
+        itemCount: loadedProducts.length,
+      ),
+    );
   }
 }
