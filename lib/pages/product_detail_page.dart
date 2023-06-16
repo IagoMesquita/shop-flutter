08516bfb4be9b07a51b044ab94fc484a4af6ae85
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-15 21:13:23 -0300

    refactor: use not

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -7,7 +7,7 @@ class ProductDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-  final Product product = ModalRoute.of(context)?.settings.arguments  as Product;
+  final Product product = ModalRoute.of(context)!.settings.arguments  as Product;
 
     return Scaffold(
       appBar: AppBar(
