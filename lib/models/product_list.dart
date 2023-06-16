e15bba04bcca5ed1f10e632f7980d713fbb3abf8
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-15 21:12:00 -0300

    feat: class with ChangeNotifier

---
+++ lib/models/product_list.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import '../data/dummy_data.dart';
+import './product.dart';
+
+class ProductList with ChangeNotifier {
+  final List<Product> _items = dummyProducts;
+
+  // [..._items] é um clone de _items. Se passasse _items no get, seria uma referencia, que poderia ser alterada por qlq um
+  List<Product> get items => [..._items];
+
+  void addProduct(Product product) {
+    _items.add(product);
+    notifyListeners();
+  }
+
+  void removeProduct(Product product) {
+    _items.remove(product);
+    notifyListeners();
+  }
+}
