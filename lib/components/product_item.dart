0571e442138c73ed2598dff6f97463123898a838
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-16 08:20:12 -0300

    refactor: consumer Provider

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/product.dart';
-import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
-  final Product product;
-
-  const ProductItem({super.key, required this.product});
+  const ProductItem({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final Product product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.favorite),
+            onPressed: () {
+              product.toggleFavorite();
+            },
+            icon: Icon(
+                product.isFarovite ? Icons.favorite : Icons.favorite_border),
             // color: Theme.of(context).accentColor,
             color: Theme.of(context).colorScheme.secondary,
           ),
