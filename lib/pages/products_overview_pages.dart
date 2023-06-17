779ad9d5af463b298ce4939e33da9a8604c50602
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-17 08:25:02 -0300

    feat: PopupMenuButton

--- lib/pages/products_overview_pages.dart
+++ lib/pages/products_overview_pages.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 
 import '../components/product_grid.dart';
 
+
+enum FilterOptions {
+  Favorite,
+  All
+}
+
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({super.key});
 
@@ -10,9 +16,25 @@ class ProductsOverviewPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Center(child: Text('Minha Loja')),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOptions.Favorite,
+                child: Text('Somente Favoritos'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.All,
+                child: Text('Todos'),
+              )
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              debugPrint(selectedValue.toString());
+            },
+          ),
+        ],
       ),
       body: const ProductGrid(),
     );
   }
 }
-
