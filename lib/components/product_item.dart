e6f3aa2a48352f03544bf8062489324fc5d2ec50
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-17 08:24:20 -0300

    refactor: use Consumer

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -8,20 +8,24 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Product product = Provider.of<Product>(context);
+    // final Product product = Provider.of<Product>(context);
+    final Product product = Provider.of<Product>(context, listen: false);
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            onPressed: () {
-              product.toggleFavorite();
-            },
-            icon: Icon(
-                product.isFarovite ? Icons.favorite : Icons.favorite_border),
-            // color: Theme.of(context).accentColor,
-            color: Theme.of(context).colorScheme.secondary,
+          leading: Consumer<Product>(
+            builder: (ctx, product, _) => IconButton(
+              onPressed: () {
+                product.toggleFavorite();
+              },
+              icon: Icon(
+                  product.isFarovite ? Icons.favorite : Icons.favorite_border),
+              // color: Theme.of(context).accentColor,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
           ),
           title: Text(product.title, textAlign: TextAlign.center),
           trailing: IconButton(
