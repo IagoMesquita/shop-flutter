39e916e3e965f29f3a0ee7c2dad18fe0272a912d
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-19 20:43:25 -0300

    feat: Card Total in Cart

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -7,7 +7,43 @@ class CartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Carrinho de compras'),
+        centerTitle: true,
+        title: const Text('Carrinho'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: const EdgeInsets.all(25),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Total',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  const SizedBox(width: 10,),
+                  Chip(
+                    label: Text('R\$1000'),
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    labelStyle: TextStyle(
+                        color:
+                            Theme.of(context).primaryTextTheme.titleLarge?.color),
+                  ),
+                  const Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text('COMPRAR'),
+                    style: TextButton.styleFrom(
+                      textStyle: const TextStyle(fontSize: 20)
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
