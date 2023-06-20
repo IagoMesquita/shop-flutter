a9a77b935ae07ca2bd4860069baaf57fce018d44
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-20 08:35:57 -0300

    feat: details product

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -3,15 +3,45 @@ import 'package:shop/models/product.dart';
 
 class ProductDetailPage extends StatelessWidget {
   const ProductDetailPage({super.key});
-  
 
   @override
   Widget build(BuildContext context) {
-  final Product product = ModalRoute.of(context)!.settings.arguments  as Product;
+    final Product product =
+        ModalRoute.of(context)!.settings.arguments as Product;
 
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(product.name),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            'R\$${product.price}',
+            style: const TextStyle(
+              color: Colors.grey,
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Text(
+              product.description,
+              textAlign: TextAlign.center,
+            ),
+          )
+        ],
       ),
     );
   }
