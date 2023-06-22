48f312bc5dd21088a31e080fd834ac81f5b6cd36
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-22 08:50:20 -0300

    feat: undo last action by SnackBar

--- lib/components/product_grid_item.dart
+++ lib/components/product_grid_item.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -43,7 +41,7 @@ class ProductGridItem extends StatelessWidget {
                   action: SnackBarAction(
                     label: 'DESFAZER',
                     onPressed: () {
-                      print('DESFAZER');
+                      cart.removeSingleItem(product.id);
                     },
 
                   ) ,
