0349e6d69d633fa2db0333037b4a29e854a89c62
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-25 11:22:08 -0300

    feat: action in appBar

--- lib/pages/products_pages.dart
+++ lib/pages/products_pages.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/product_item.dart';
+import 'package:shop/utils/app_routes.dart';
 
 import '../models/product_list.dart';
 import '../components/app_drawer.dart';
@@ -16,11 +17,19 @@ class ProductsPage extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Gerenciar Produtos'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(AppRoutes.PRODUCT_FORM);
+            },
+            icon: const Icon(Icons.add),
+          )
+        ],
       ),
       drawer: const AppDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(8),
-        child: ListView.builder( 
+        child: ListView.builder(
           itemCount: products.itemCout,
           itemBuilder: (ctx, index) => Column(
             children: [
