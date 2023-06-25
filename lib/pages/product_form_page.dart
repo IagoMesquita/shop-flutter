b9d02fb10651b19ff330bb397b7b75e01e64991e
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-25 11:54:09 -0300

    feat: controlling Focus manually

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
-class ProductFormPage extends StatelessWidget {
+class ProductFormPage extends StatefulWidget {
   const ProductFormPage({super.key});
 
   @override
+  State<ProductFormPage> createState() => _ProductFormPageState();
+}
+
+class _ProductFormPageState extends State<ProductFormPage> {
+    final _priceFocus = FocusNode();
+
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -17,11 +25,15 @@ class ProductFormPage extends StatelessWidget {
           children: [
             TextFormField(
               decoration: const InputDecoration(labelText: 'Nome'),
-              textInputAction: TextInputAction.next,              
+              textInputAction: TextInputAction.next,        
+              onFieldSubmitted: (_) {
+                FocusScope.of(context).requestFocus(_priceFocus);
+              },        
             ),
             TextFormField(
               decoration: const InputDecoration(labelText: 'Preço'),
               keyboardType: const TextInputType.numberWithOptions(decimal: true),
+              focusNode: _priceFocus,
             ),
           ],
         )),
