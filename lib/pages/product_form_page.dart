fc51febe8234ddaa970e7c2939b17ce9d29f82e0
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-25 11:22:30 -0300

    feat: new page Product Form

---
+++ lib/pages/product_form_page.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class ProductFormPage extends StatelessWidget {
+  const ProductFormPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Formulário de Produto'),
+      ),
+    );
+  }
+}
