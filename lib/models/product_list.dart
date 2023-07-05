690b4f6cb7c7eaee1437246b77a75f24ad7f3059
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-07-04 23:34:00 -0300

    refactor: change methodes for Future

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -20,7 +20,7 @@ class ProductList with ChangeNotifier {
     return _items.length;
   }
 
-  void saveProduct(Map<String, Object> data) {
+  Future<void> saveProduct(Map<String, Object> data) {
     bool hasId = data['id'] != null;
 
     final product = Product(
@@ -32,13 +32,13 @@ class ProductList with ChangeNotifier {
     );
 
     if (hasId) {
-      updateProduct(product);
+      return updateProduct(product);
     } else {
-      addProduct(product);
+     return addProduct(product);
     }
   }
 
-  void addProduct(Product product) {
+  Future<void> addProduct(Product product) {
     final future = http.post(Uri.parse('$_baseUrl/products.json'),
         body: jsonEncode(
           {
@@ -50,7 +50,7 @@ class ProductList with ChangeNotifier {
           },
         ));
 
-    future.then((response) {
+    return future.then<void>((response) {
       final id = jsonDecode(response.body)['name'];
       _items.add(
         Product(
@@ -66,18 +66,19 @@ class ProductList with ChangeNotifier {
     });
   }
 
-  void updateProduct(Product product) {
+ Future<void> updateProduct(Product product) {
     int index = _items.indexWhere((p) => p.id == product.id);
     if (index >= 0) {
       _items[index] = product;
       notifyListeners();
     }
+
+    return Future.value();
   }
 
   void removeProduct(Product product) {
     int index = _items.indexWhere((p) => p.id == product.id);
 
-    print('Index: $index');
     if (index >= 0) {
       _items.removeWhere((p) => p.id == product.id);
       notifyListeners();
