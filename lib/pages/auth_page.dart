4736ba6186e89c80e03ed90ca1b9ccfaa2425418
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-07-10 08:03:04 -0300

    feat: logo shop

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatelessWidget {
@@ -9,12 +11,49 @@ class AuthPage extends StatelessWidget {
       body: Stack(children: [
         Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromRGBO(215, 117, 255, 0.5),
-                Color.fromRGBO(255, 188, 117, 0.9),
-              ],
-            ),
+            gradient: LinearGradient(colors: [
+              Color.fromRGBO(215, 117, 255, 0.5),
+              Color.fromRGBO(255, 188, 117, 0.9),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 70,
+                ),
+                transform: Matrix4.rotationZ(-8 * pi / 180),
+                // cascade operator .. 
+                // transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.deepOrange.shade900,
+                  boxShadow: const [
+                    BoxShadow(
+                      blurRadius: 8,
+                      color: Colors.black26,
+                      offset: Offset(0, 2),
+                    )
+                  ],
+                ),
+                child: const Text(
+                  'Minha Loja',
+                  style: TextStyle(
+                      fontSize: 45,
+                      fontFamily: 'Anton',
+                      fontWeight: FontWeight.w900,
+                      color: Colors.white
+                      // color: Theme.of(context).accentTextTheme.headlineLarge?.color
+                      ),
+                ),
+              )
+            ],
           ),
         )
       ]),
