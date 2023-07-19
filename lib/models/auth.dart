4e0d6c1c9eede9fa1668109c3f6d1661ea3415c3
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-07-18 21:43:09 -0300

    feat: throw error

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -2,11 +2,9 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop/exceptions/auth_exception.dart';
 
 class Auth with ChangeNotifier {
-  static const _url =
-      'https://identitytoolkit.googleapis.com/v1/accounts:?key=AIzaSyCdAiC5URnx8alehCOTvG4phywQrOkwOsM';
-
   Future<void> _authenticate (
       String email, String password, String urlFragment) async{
     final url =
@@ -23,15 +21,21 @@ class Auth with ChangeNotifier {
       ),
     );
 
-    print(jsonDecode(response.body));
+    final body = jsonDecode(response.body);
+
+    if(body['error'] != null) {
+      throw AuthException(key: body['error']['message']);
+    }
+
+    print(body);
 
   }
 
   Future<void> singup(String email, String password) async {
-    _authenticate(email, password, 'signUp');
+    return _authenticate(email, password, 'signUp');
   }
 
     Future<void> login(String email, String password) async {
-    _authenticate(email, password, 'signInWithPassword');
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
