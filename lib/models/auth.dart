f7970e094308d158504719c6af461236252f54e5
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-07-21 08:28:29 -0300

    feat: auth

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -5,8 +5,34 @@ import 'package:http/http.dart' as http;
 import 'package:shop/exceptions/auth_exception.dart';
 
 class Auth with ChangeNotifier {
-  Future<void> _authenticate (
-      String email, String password, String urlFragment) async{
+  String? _token;
+  String? _email;
+  String? _uid;
+  DateTime? _expiryDate;
+
+  bool get isAuth {
+    final isValid = _expiryDate?.isAfter(DateTime.now()) ?? false;
+
+    return _token != null && isValid;
+  }
+
+  String? get token {
+    return isAuth ? _token : null;
+  }
+
+  String? get email {
+    return isAuth ? _email : null;
+  }
+
+  String? get uid {
+    return isAuth ? _uid : null;
+  }
+
+  Future<void> _authenticate(
+    String email,
+    String password,
+    String urlFragment,
+  ) async {
     final url =
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?key=AIzaSyCdAiC5URnx8alehCOTvG4phywQrOkwOsM';
 
@@ -23,19 +49,29 @@ class Auth with ChangeNotifier {
 
     final body = jsonDecode(response.body);
 
-    if(body['error'] != null) {
+    if (body['error'] != null) {
       throw AuthException(key: body['error']['message']);
+    } else {
+      _token = body['idToken'];
+      _email = body['email'];
+      _uid = body['localId'];
+
+      _expiryDate = DateTime.now().add(
+        Duration(
+          seconds: int.parse(body['expiresIn']),
+        ),
+      );
+      notifyListeners();
     }
 
     print(body);
-
   }
 
   Future<void> singup(String email, String password) async {
     return _authenticate(email, password, 'signUp');
   }
 
-    Future<void> login(String email, String password) async {
+  Future<void> login(String email, String password) async {
     return _authenticate(email, password, 'signInWithPassword');
   }
 }
