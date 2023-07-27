516379bdb368dc9f1f22005f8ac24c646fbddde3
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-07-27 20:13:41 -0300

    feat: autoLogout

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -9,6 +10,7 @@ class Auth with ChangeNotifier {
   String? _email;
   String? _userId;
   DateTime? _expiryDate;
+  Timer? _logoutTimer;
 
   bool get isAuth {
     final isValid = _expiryDate?.isAfter(DateTime.now()) ?? false;
@@ -61,6 +63,9 @@ class Auth with ChangeNotifier {
           seconds: int.parse(body['expiresIn']),
         ),
       );
+
+      _autoLogout();
+
       notifyListeners();
     }
 
@@ -80,7 +85,21 @@ class Auth with ChangeNotifier {
     _email = null;
     _userId = null;
     _expiryDate = null;
+    _clearLogoutTimer();
+    notifyListeners();
+  }
 
+  void _clearLogoutTimer() {
+    _logoutTimer?.cancel();
+    _logoutTimer = null;
+  }
+
+  void _autoLogout() {
+    _clearLogoutTimer();
+
+    final timeToLogout = _expiryDate?.difference(DateTime.now()).inSeconds;
+
+    _logoutTimer = Timer(Duration(seconds: timeToLogout ?? 0), logout);
     notifyListeners();
   }
 }
